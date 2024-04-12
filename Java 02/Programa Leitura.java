ee08a2996d14bb8863ba456bdab2d2c97ec27132
Gessica Vitorio <143418965+Gessvi@users.noreply.github.com>
2024-04-12 08:33:16 -0300

    Update Programa Leitura.java

--- Java 02/Programa Leitura.java
+++ Java 02/Programa Leitura.java
@@ -1,42 +1,37 @@
 package aplicacao;
-
 import java.util.Scanner;
-
 public class Programa {
 
-    public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
+	public static void main (String[] args) {
+	Scanner leia = new Scanner (System.in);
+	Produto produto = new Produto();
+	
+	System.out.println ("Insira o Nome do Produto: ");
+		produto.produto = leia.next();
+	
+	System.out.println ("Insira a Quantidade no Estoque: ");
+		produto.quantidade = leia.nextDouble();
+		
+	System.out.println ("Insira o Preço do Produto: ");
+		produto.preco = leia.nextDouble();
+		
+	System.out.println ("Produto: " +produto.produto+ " Estoque Atual: " +produto.quantidade+ " Preço: " +produto.preco);
+	
+	System.out.println ("Insira a Quantidade de Produtos Adicionais: ");
+		int estoque = leia.nextInt();
+		produto.adcProduto(estoque);
+		
+	System.out.println("Atualização: " +produto.produto+ " Estoque Atual: " +produto.quantidade+ " Preço: " +produto.preco);
 
-        Produto produto = new Produto();
-
-        System.out.println("Insira o produto cadastrado: ");
-        produto.produto = leia.next();
-
-        System.out.println("Insira a quantidade de produtos no estoque: ");
-        produto.quantidade = leia.nextDouble();
-
-        System.out.println("Insira o preço individual: ");
-        produto.preco = leia.nextDouble();
-
-        System.out.println("Confirmacao: " + produto.produto + " estoque atual: " + produto.quantidade + " Preco: "
-                + produto.preco);
-
-        System.out.println("Insira a quantidade de produtos adicionais no estoque: ");
-        int estoque = leia.nextInt();
-        produto.adcProduto(estoque);
-
-        System.out.println("Atualização: " + produto.produto + ", Estoque Atual: " + produto.quantidade + ", preco: "
-                + produto.preco);
-
-        System.out.println("Quantidade de vendas: ");
-        estoque = leia.nextInt();
-        produto.subProduto(estoque);
-
-        System.out.println("Atualização pos vendas: " + produto.produto + ", Estoque " + produto.quantidade + ", preco "
-                + produto.preco);
-
-        leia.close();
-
-    }
-
+	System.out.println("Insira a Quantidade de Vendas: ");
+	estoque = leia.nextInt();
+	produto.subProduto(estoque);
+	
+	System.out.println("Atualização pos Vendas: " +produto.produto+ " Estoque Atual: " +produto.quantidade+ " Preço: " +produto.preco);
+	double lucro;
+	lucro = produto.preco * estoque;
+    System.out.println ("Lucro Financeiro: " +(float)lucro);
+	
+	leia.close();
+	}
 }
